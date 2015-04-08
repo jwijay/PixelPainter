1551dd83c7d0f8a33a9c1893c7cf88cf51a7e411
Jacqueline Wijaya <jackie.wijaya@gmail.com>
2015-04-07 17:15:40 -1000

    changing port to env var for heroku

--- app.js
+++ app.js
@@ -72,10 +72,10 @@ app.post('/', function(req, res) {
   });
 });
 
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function() {
+  // var host = server.address().address;
+  // var port = server.address().port;
 
-var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('PixelPainter app listening at http://%s:%s', host, port);
+  // console.log('PixelPainter app listening at http://%s:%s', host, port);
 });
